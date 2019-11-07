c232299224f9db537b70c098a2dff3c5524b3efe
Aleem Pathammavong <Aleemp321@yahoo.com>
2019-11-07 15:10:25 -0700

    basic objective complete

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -11,26 +11,103 @@ class Menu
 end
 
 class Food 
+  attr_accessor :main, :side
+
   def initialize
     @main = ["Steak", "Salmon"]
     @side = ["Vegatables", "Mash Potatoes", "French Fries"]
+    
+  end 
+end 
+
+class Customer 
+
+  attr_accessor :meal
+  def initialize
     @meal = [] 
   end 
 end 
 
 class Lunch_lady < Food 
-  def initialize 
-    puts "Welcome to Lunch_Lady, What would you like?"
+  def initialize
+    @food = Food.new 
+    @customer = Customer.new 
+    puts "Welcome to Lunch_Lady"
     order 
   end 
 
-  def main_dish
+  def create_meal
+    puts "Select a Main Dish"
+    @food.main.each_with_index { |food, index|
+      puts "#{index + 1}) #{food}" }
+    
+    selection = gets.to_i 
 
+    case selection 
+      when 1 
+        @customer.meal << @food.main[selection - 1]
+        side_dish
+      when 2
+        @customer.meal << @food.main[selection - 1]
+        side_dish
+      else
+        puts "invalid option"
+        create_meal
+    end 
   end
 
   def side_dish
+    puts "Select 2 side dishes"
+    @food.side.each_with_index { |food, index|
+    puts "#{index + 1}) #{food}" }
+
+    @second = gets.to_i 
+
+    case @second 
+      when 1 
+        @customer.meal << @food.side[@second - 1]
+        @food.side.delete_at(@second - 1)
+        second_choice
+      when 2
+        @customer.meal << @food.side[@second - 1]
+        @food.side.delete_at(@second - 1)
+        second_choice
+      when 3
+        @customer.meal << @food.side[@second - 1]
+        @food.side.delete_at(@second - 1)
+        second_choice
+      else
+        puts "invalid option"
+        side_dish 
+    end 
 
   end
+
+  def second_choice 
+    puts "Please select another option"
+    @food.side.each_with_index { |food, index|
+    puts "#{index + 1}) #{food}" }
+
+    second_choice = gets.to_i 
+
+    case second_choice
+      when 1
+        @customer.meal << @food.side[second_choice - 1]
+        total
+      when 2
+        @customer.meal << @food.side[second_choice - 1]
+        total
+      else 
+        puts "Invalid Choice"
+      end 
+  end 
+
+  def total
+    @customer.meal.each do |lunch|
+      puts "You will be having.. #{lunch}" 
+    end
+      puts "A total of... #{@customer.meal.length} items"
+  end 
 
   def order
     menu = Menu.new 
@@ -40,13 +117,11 @@ class Lunch_lady < Food
 
     case choice 
       when 1 
-        main_dish
-        puts "Please select 2"
+        create_meal
       else 
         exit
       end
   end
-
-end
+end 
 
 Lunch_lady.new 
